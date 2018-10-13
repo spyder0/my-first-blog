dc4f3c44c6e2d5517d78bd69b973440c35e00457
spartan <20395752@qq.com>
2018-10-13 10:20:12 +0800

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+	
+'''
+return （返回）用 render 方法渲染模板 blog/post_list.html 而得到的结果.
+'''
